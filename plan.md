c116fa743b1211dd072085a090ac2a21aaf15721
AbhishekPratap05 <pratap.5dec@gmail.com>
2020-09-20 16:01:25 +0530

    Initialize our first view ✔️ Create a room with unique ID ✔️

--- plan.md
+++ plan.md
@@ -1,8 +1,8 @@
 #Plan of Action
 
 - [X] Initialize NodeJS Project
-- [ ] Initialize our first view
-- [ ] Create a room with unique ID
+- [X] Initialize our first view
+- [X] Create a room with unique ID
 - [ ] Add the ability to view our own video
 - [ ] Add ability to allow others to stream their video
 - [ ] Add styling
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+//importing v4 version of uuid to make rooms id
+const {v4:uuidv4} = require('uuid');
+
+app.set('view engine','ejs');
+
+app.get('/', (request,response)=>{
+    response.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:room',(request,response)=>{
+    response.render('room',{roomId:request.params.room})
+})
+
+
+server.listen(3030);
